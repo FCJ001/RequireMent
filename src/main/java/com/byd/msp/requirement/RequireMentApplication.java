5c160368c44d72d7e5c0e859b575f00e513e6ae8
chunjiu <1614949142@qq.com>
2026-05-09 21:35:53 +0800

    初始化 Spring Boot 项目骨架
    Spring Boot 2.7.17 + Nacos + PostgreSQL + MyBatis-Plus + Flowable

---
+++ src/main/java/com/byd/msp/requirement/RequireMentApplication.java
@@ -0,0 +1,17 @@
+package com.byd.msp.requirement;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients(basePackages = "com.byd.xjsy.common.feign")
+@MapperScan("com.byd.msp.requirement.mapper")
+public class RequireMentApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RequireMentApplication.class, args);
+    }
+}
